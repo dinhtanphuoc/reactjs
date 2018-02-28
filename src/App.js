82410da149459a9b74a5d3cde277af2348afff68
dinhtanphuoc <dinhtanphuoc01@gmail.com>
2018-02-28 15:00:08 +0700

    Cach viet Reactjs Can phu thuoc xu ly component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import { HomePage } from './containers';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
+          <HomePage />
         </header>
       </div>
     );
   }
 }
-
-export default App;
